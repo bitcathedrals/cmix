a3bd0bb13c4cbe281015104c7acbc4320c17960b
Michael Mattie <codermattie@runbox.com>
2026-05-17 16:59:28 -0700

    (dev): create a type for the index registers

--- src/cpu/short.h
+++ src/cpu/short.h
@@ -1,8 +1,10 @@
 #ifndef CPU_SHORT_H
 #define CPU_SHORT_H
 
-static const byte address_size = 2;
-static const byte address_begin = 0;
-static const byte address_end = 1;
+using index_t = signed short int;
+
+static const index address_size = 2;
+static const index address_begin = 0;
+static const index address_end = 1;
 
 #endif
