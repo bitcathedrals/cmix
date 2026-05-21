2809bf2c2ed674b2c4c305d8ef1b842b41555db5
Michael Mattie <codermattie@runbox.com>
2026-05-21 10:17:48 -0700

    (dev): temp commit of exception work

--- src/exception.h
+++ src/exception.h
@@ -2,13 +2,39 @@
 #define CPU_EXCEPTION_H
 
 #include <iostream>
+#include <sstream>
+#include <utility>
 #include <string>
 #include <chrono>
 #include <stdexcept>
+#include <memory>
 
-class GeneralException : public std::runtime_error {
+using ThrowablePointer = std::unique_ptr<std::ostreamstring>;
+
+class ThrowableStream : public std::runtime_error {
+    explicit ThrowableStream(std::string context) : runtime_error(context),
+                                                    stream(new ostringstream) {}
+
+    ThrowableStream& operator=(ThrowableStream&& prev) {
+        stream = std::move(prev.stream);
+        return *this;
+    }
+
+    std::string& str(void) { return stream.str(); }
+
+    ThrowablePointer* operator->() {
+        return stream;
+    }
+
+private:
+    ThrowableStream() {}
+
+    ThrowablePointer stream;
+}
+
+class GeneralException : public ThrowableStream {
 public:
-    GeneralException(std::string context) : runtime_error(context),
+    GeneralException(std::string context) : ThrowableStream(context),
                                             context(context),
                                             timestamp(std::chrono::system_clock::now()) {
     }
@@ -23,9 +49,9 @@ public:
 std::ostream& operator<<(std::ostream& output,
                          const GeneralException& general);
 
-class ArithmeticException : public std::runtime_error {
+class ArithmeticException : public std::ThrowableStream {
 public:
-    ArithmeticException(std::string context, int x) : std::runtime_error(context), x(x) {}
+    ArithmeticException(std::string context, int x) : ThrowableStream(context), x(x) {}
 
     friend std::ostream& operator<<(std::ostream& output,
                                     const ArithmeticException& ex);
