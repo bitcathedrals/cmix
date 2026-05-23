6ae2a7dd545e0214aa7f5eca99ab6c44ee136bd5
Michael Mattie <codermattie@runbox.com>
2026-05-23 10:58:09 -0700

    (dev): add cpu copy constructor test

--- tests/cpu/cpu_test.cpp
+++ tests/cpu/cpu_test.cpp
@@ -3,6 +3,31 @@
 #include <cpu/word.h>
 #include <cpu/cpu.h>
 
+
+//
+// test memory array, type and set
+//
+
+TEST(CpuData, CopyCPU) {
+    for(auto i=0;i < memory_capacity; i++) {
+        CPU.memory[i][0] = 1;
+        CPU.memory[i][1] = 2;
+        CPU.memory[i][2] = 3;
+        CPU.memory[i][3] = 4;
+        CPU.memory[i][4] = 5;
+    }
+
+    Cpu copy(CPU);
+
+    for(auto i=0;i < memory_capacity; i++) {
+        ASSERT_EQ(copy.memory[i][0], 1);
+        ASSERT_EQ(copy.memory[i][1], 2);
+        ASSERT_EQ(copy.memory[i][2], 3);
+        ASSERT_EQ(copy.memory[i][3], 4);
+        ASSERT_EQ(copy.memory[i][4], 5);
+    }
+}
+
 //
 // test memory array, type and set
 //
