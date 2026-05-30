1ce7418b3205a4e343fcfef1703623634c334279
Michael Mattie <codermattie@runbox.com>
2026-05-30 15:42:40 -0700

    (dev): rework interface for debug using repl

--- debug/debug.cpp
+++ debug/debug.cpp
@@ -4,6 +4,8 @@
 #include <parse.h>
 #include <edit.h>
 
+#include <repl/dispatch.h>
+
 int main() {
     std::cerr << "Welcome to cmix Debugging, down the rabbit hole we go!" << std::endl;
 
@@ -19,9 +21,8 @@ int main() {
 
         CmdStatus status = run_command(parse);
 
-        std::cerr << "ran command - got: "
-                  << cmd_status_string(status)
-                  << std::endl;
+        std::cerr << cmd_string(parse) << " command terminated with status: "
+                  << status_string(status) << std::endl;
     } while(true);
 
     return 0;
