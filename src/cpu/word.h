0a19cce2d4479665dbe57aa0dd0081a6a99f3108
Michael Mattie <codermattie@runbox.com>
2026-05-29 18:02:18 -0700

    (dev): move over Word constants to inline constexpr

--- src/cpu/word.h
+++ src/cpu/word.h
@@ -13,31 +13,31 @@
 
 inline constexpr byte word_size = 6;
 
-static const byte data_bits = 6;
+inline constexpr byte data_bits = 6;
 
-static const byte positive_max = 64;
-static const byte negative_max = -64;
+inline constexpr byte positive_max = 64;
+inline constexpr byte negative_max = -64;
 
 //
 // data = word - sign
 //
 
-static const byte data_size = 5;
+inline constexpr byte data_size = 5;
 
-static const byte data_min = 1;
-static const byte data_max = 5;
+inline constexpr byte data_min = 1;
+inline constexpr byte data_max = 5;
 
-static const byte data_offset = 1;
+inline constexpr byte data_offset = 1;
 
 //
 // sign is a -1 || 1 depending on the sign.
 //
 
-static const byte sign_field = 0;
-static const byte sign_default = 1;
+inline constexpr byte sign_field = 0;
+inline constexpr byte sign_default = 1;
 
-static const byte sign_positive = 1;
-static const byte sign_negative = -1;
+inline constexpr byte sign_positive = 1;
+inline constexpr byte sign_negative = -1;
 
 class Word {
 public:
