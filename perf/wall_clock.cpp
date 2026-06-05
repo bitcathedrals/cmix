b5a49415188066be929712b7a830412fab3523f1
Michael Mattie <codermattie@runbox.com>
2026-06-05 11:57:21 -0700

    (dev): working timestamp + nanosecond delta

--- perf/wall_clock.cpp
+++ perf/wall_clock.cpp
@@ -1,17 +1,38 @@
+#include <iostream>
+#include <chrono>
+#include <format>
+
 #include <wall_clock.h>
 
-#include <iostream>
+namespace clk = std::chrono;
 
-Clocked::Clocked(std::string name) : name(name),
-                                     start(std::chrono::steady_clock::now()) {}
+Clocked::Clocked(std::string name) : name(name) {
+    get_time(start_tick, start_time);
+}
+
+void Clocked::get_time(clocked_tick& tick, clocked_time& time) {
+    tick = clk::steady_clock::now();
+    time = clk::system_clock::now();
+}
+
+const std::chrono::local_time<std::chrono::nanoseconds> Clocked::timestamp(const clocked_time time) const {
+    const auto wall_ns = clk::time_point_cast<clk::nanoseconds>(time);
+
+    const auto local_zone = clk::current_zone();
+    return local_zone->to_local(wall_ns);
+}
 
 Clocked::~Clocked() {
-    auto end = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    clocked_tick end_tick;
+    clocked_time end_time;
+
+    get_time(end_tick, end_time);
+
+    auto delta = clk::duration_cast<clk::nanoseconds>(end_tick - start_tick);
 
     std::cerr << "Clocked: " << name
-//              << " Start = " << start
-//              << " End = " << end
-              << " Elapsed = " << elapsed
+              << " Start = " << timestamp(start_time)
+              << " Elapsed = " << delta.count() << "ns"
+              << " End = " << timestamp(end_time)
               << std::endl;
 }
--- perf/wall_clock.h
+++ perf/wall_clock.h
@@ -4,15 +4,23 @@
 #include <string>
 #include <chrono>
 
+using clocked_tick = std::chrono::steady_clock::time_point;
+using clocked_time = std::chrono::system_clock::time_point;
+
 class Clocked {
 public:
     Clocked(std::string name);
     ~Clocked();
 
 private:
+    void get_time(clocked_tick& tick, clocked_time& time);
+
+    const std::chrono::local_time<std::chrono::nanoseconds> timestamp(const clocked_time time) const;
+
     std::string name;
 
-    std::chrono::steady_clock::time_point start;
+    clocked_tick start_tick;
+    clocked_time start_time;
 };
 
 #endif
