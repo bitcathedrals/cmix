38728748c3e6cf68cd45ee23cd0332a39dfb1dc0
Michael Mattie <codermattie@runbox.com>
2026-06-07 20:39:57 -0700

    (dev): partially fix libedit

--- src/edit.cpp
+++ src/edit.cpp
@@ -1,29 +1,46 @@
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
-#include <editline/readline.h>
+#include "edit.h"
+#include <histedit.h>
 
-static const char* prompt_string = "cmix> ";
+extern "C" {
+    static std::string prompt("cmix> ");
 
-std::string get_input() {
-    std::string result;
+    static EditLine* el = nullptr;
 
-    char* input = nullptr;
-
-    // Loop continuously to read user input
-    while ((input = readline(prompt_string)) != NULL) {
-        if (input[0] != '\0') {
-            add_history(input);
-
-            result = input;
-
-            free(input);
-
-            break;
-        }
-
-        std::cerr << "cmix: error, try again." << std::endl;
+    char* edit_prompt(EditLine* el [[maybe_unused]]) {
+        return const_cast<char*>("buggy> ");
     }
 
-    return result;
+    void setup_lib_edit() {
+        el = el_init((const char*)"cmix", stdin, stdout, stderr);
+
+        if(!el) {
+            std::cerr << "el_init failed" << std::endl;
+            std::exit(1);
+        }
+
+        el_set(el, EL_PROMPT, &edit_prompt);
+        el_set(el, EL_EDITOR, "emacs");
+    }
+}
+
+std::string get_input(void) {
+    int count = 0;
+
+    if (el == nullptr) {
+        setup_lib_edit();
+    }
+
+    const char* line = nullptr;
+
+    while((line = el_gets(el, &count)) != nullptr) {
+        if(count > 1) {
+            return std::string(line);
+        }
+    }
+
+    return std::string();
 }
