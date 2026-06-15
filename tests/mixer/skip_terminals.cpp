3de0ecc9f44b9c308c482394df17c77b4d24d800
Michael Mattie <codermattie@runbox.com>
2026-06-14 19:31:43 -0700

    (dev): more tests for skipping terminals

--- tests/mixer/skip_terminals.cpp
+++ tests/mixer/skip_terminals.cpp
@@ -3,10 +3,10 @@
 #include "mixer/parser.h"
 
 TEST(MixerBasicToken, NumberAfterTerminals) {
-    std::string lower("  22");
+    std::string blank_then_number("  22");
 
-    std::string::const_iterator begin = lower.cbegin();
-    std::string::const_iterator end = lower.cend();
+    std::string::const_iterator begin = blank_then_number.cbegin();
+    std::string::const_iterator end = blank_then_number.cend();
 
     Numeric matcher(Token::label::number);
 
@@ -14,4 +14,34 @@ TEST(MixerBasicToken, NumberAfterTerminals) {
 
     EXPECT_EQ(token.get_type(), Token::label::number);
     EXPECT_EQ(token.get_match().size(), static_cast<size_t>(2));
+};
+
+
+TEST(MixerBasicToken, EmptyString) {
+    std::string empty("");
+
+    std::string::const_iterator begin = empty.cbegin();
+    std::string::const_iterator end = empty.cend();
+
+    Numeric matcher(Token::label::number);
+
+    Token token = matcher.match(begin, end);
+
+    EXPECT_EQ(token.get_type(), Token::label::nothing);
+    EXPECT_EQ(token.get_match().size(), static_cast<size_t>(0));
+};
+
+
+TEST(MixerBasicToken, AillTerminals) {
+    std::string empty("  ");
+
+    std::string::const_iterator begin = empty.cbegin();
+    std::string::const_iterator end = empty.cend();
+
+    Numeric matcher(Token::label::number);
+
+    Token token = matcher.match(begin, end);
+
+    EXPECT_EQ(token.get_type(), Token::label::nothing);
+    EXPECT_EQ(token.get_match().size(), static_cast<size_t>(0));
 };
