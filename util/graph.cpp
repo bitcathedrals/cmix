cd6ffa630e462977ff0479cd748a574ef26a55bf
Michael Mattie <codermattie@runbox.com>
2026-07-23 19:08:25 -0700

    (dev): basic command loop working

--- util/graph.cpp
+++ util/graph.cpp
@@ -5,10 +5,19 @@
 #include "mixer/parser.h"
 #include "mixer/address.h"
 
-int main(const int argc [[maybe_unused]], const char* argv[]) {
+int main(const int argc, const char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "address_comma_index" << std::endl;
+        return 0;
+    }
+
     std::string address_comma_index("address_comma_index");
 
     if(address_comma_index == argv[1]) {
         build_comma_index_parser()->graph(std::cout);
+        return 0;
     }
+
+    std::cerr << "util/graph: no command found for: " << argv[1] << std::endl;
+    return 1;
 }
