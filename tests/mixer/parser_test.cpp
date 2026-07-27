e2472a766af1dcc8e33c2b329f65e720274262f7
Michael Mattie <codermattie@runbox.com>
2026-07-26 18:18:23 -0700

    (dev): simple comma index parser done

--- tests/mixer/parser_test.cpp
+++ tests/mixer/parser_test.cpp
@@ -141,6 +141,6 @@ TEST(MixerParserAddress, AddressCommaIndexOk) {
     EXPECT_EQ(p[0][0].get_token(), "1234");
 
     EXPECT_EQ(p[1].get_type(), Token::label::node);
-    // EXPECT_EQ(p[1][0].get_type(), Token::label::special);
-    // EXPECT_EQ(p[1][1].get_type(), Token::label::number);
+    EXPECT_EQ(p[1][0].get_type(), Token::label::special);
+    EXPECT_EQ(p[1][1].get_type(), Token::label::number);
 }
