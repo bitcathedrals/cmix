8efef5961b2445c298225dfd17676e2f6722d8f7
Michael Mattie <codermattie@runbox.com>
2026-07-28 20:02:40 -0700

    (dev): test parser addresss only

--- tests/mixer/parser_address_test.cpp
+++ tests/mixer/parser_address_test.cpp
@@ -4,6 +4,21 @@
 #include "mixer/parser.h"
 #include "mixer/address.h"
 
+TEST(MixerAddress, AddressOnly) {
+    std::string test_string("1234");
+
+    auto p = Token::descent(build_address_parser(), test_string);
+
+    EXPECT_EQ(p[0].get_type(), Token::label::node);
+
+    auto address = p.walk("address_address/address_number");
+
+    ASSERT_NE(address, nullptr);
+
+    EXPECT_EQ(address->get_type(), Token::label::number);
+    EXPECT_EQ(address->get_token(), "1234");
+}
+
 TEST(MixerAddress, CommaAddressIndexCorrect) {
     std::string test_string("1234,4");
 
