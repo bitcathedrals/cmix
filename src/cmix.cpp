5da6104902432184b010dbc0f05065c5b02e99f1
Michael Mattie <codermattie@runbox.com>
2026-08-10 13:35:49 -0700

    (dev): fix removed cmd_string function

--- src/cmix.cpp
+++ src/cmix.cpp
@@ -21,7 +21,7 @@ int main() {
 
         if (cmd == Command::unknown) {
             std::cerr << "cmix: unknown command: "
-                      << cmd_string(parse)
+                      << parse
                       << std::endl;
             continue;
         }
@@ -32,7 +32,8 @@ int main() {
 
         CmdStatus status = run_command(parse);
 
-        std::cerr << cmd_string(parse) << " command terminated with status: "
+        std::cerr << parse
+                  << " command terminated with status: "
                   << status_string(status) << std::endl;
     } while(true);
 
